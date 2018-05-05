851b66183e3cf9be6afbc45496507b99fd02dfdf
ritikakh <ritikakh@usc.edu>
2018-05-05 11:22:32 -0700

    Fetching data from Youtube API

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />
-};
+// const SearchBar = () => {
+//     return <input />
+// };
+
+class SearchBar extends Component{
+    constructor(props) {
+        super(props); //calling the parent method of Component
+
+        this.state = {next: 'search'}; //initiallizing state
+    }
+    render () {
+        return (
+            <div>
+                <input
+                    value = {this.state.next}
+                    onChange={event => this.setState({next: event.target.value})}
+                />
+            </div>
+        );
+    }
+}
+
+
+
+
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDnclBg8syWi8q2voyc6PD5kqOfINLJPQs';
 
+
+
 //Create a compnent
-const App = () => {
-    return (<div>
-        <SearchBar />
-    </div>);
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: []};
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
 }
 
 //Put the component in DOM
