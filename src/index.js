6334947a2ab8c65d2c2a7d7b67c365b5fda0552e
ritikakh <ritikakh@usc.edu>
2018-05-04 22:11:32 -0700

    Javascript function based component search_bar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyDnclBg8syWi8q2voyc6PD5kqOfINLJPQs';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Create a compnent
+const App = () => {
+    return (<div>
+        <SearchBar />
+    </div>);
+}
+
+//Put the component in DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
