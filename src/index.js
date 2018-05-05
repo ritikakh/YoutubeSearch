8a995c953b9042374800a7ede85dbc70197a1cd4
ritikakh <ritikakh@usc.edu>
2018-05-05 12:36:00 -0700

    video working form youtube

--- src/index.js
+++ src/index.js
@@ -3,20 +3,24 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDnclBg8syWi8q2voyc6PD5kqOfINLJPQs';
 
-
-
-//Create a compnent
+//Create a component
 class App extends Component {
     constructor(props) {
         super(props);
-
-        this.state = { videos: []};
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
 
     }
@@ -24,6 +28,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList videos={this.state.videos}/>
             </div>
         );
